40032041042227c6686e14ba1c3ead7ed609c1b9
John Furtado <john.furtado@virginpulse.com>
2019-05-23 15:04:52 -0400

    immutability/mutability in Kotlin

--- src/main.kt
+++ src/main.kt
@@ -3,19 +3,28 @@ import com.rsk.Person
 fun main(args: Array<String>) {
     println("Hello, World!")
 
-    val sam = Person("sam")
+//    val sam = Person("sam")
+//
+//    sam.displayName()
+//
+//    println("Name is ${sam.Name}")
+//
+//    sam.Name = "Samantha"
+//
+//    println("Name is ${sam.Name}")
+//
+//    sam.displyWithLambda(::printName)
 
-    sam.displayName()
-
-    println("Name is ${sam.Name}")
-
-    sam.Name = "Samantha"
-
-    println("Name is ${sam.Name}")
-
-    sam.displyWithLambda(::printName)
+    val q = Question()
 }
 
-fun printName(name: String) {
-    println(name)
+//fun printName(name: String) {
+//    println(name)
+//}
+
+class Question {
+    // mutable
+    var Answer:String = ""
+    // immutable
+    val Question:String = "What is the meaning of life?"
 }
